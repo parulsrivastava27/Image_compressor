9ba0d95c5c6e592d68433048743a79b2d1cea768
parulsrivastava27 <114804907+parulsrivastava27@users.noreply.github.com>
2024-03-25 02:25:55 +0530

    Update settings.py

--- imageFunctions/settings.py
+++ imageFunctions/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'imageFunctions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        
     }
 }
 
