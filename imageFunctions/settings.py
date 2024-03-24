6e3d67c58e15263fceb26e3f96b1fb9f1f25500a
parulsrivastava27 <114804907+parulsrivastava27@users.noreply.github.com>
2024-03-25 02:28:27 +0530

    Update settings.py

--- imageFunctions/settings.py
+++ imageFunctions/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'imageFunctions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        
-    }
-}
 
 
 # Password validation
